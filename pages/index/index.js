72797a33877538b8b3ed38a5fac63c1f5860032e
Astrall <1179303687@qq.com>
2020-05-10 17:24:47 +0800

    5.10
    增加登陆授权，不授权会一直停留在授权界面

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,10 @@ const app = getApp()
 Page({
   //初始页定在home页
   data: {   
-    PageCur: 'home'
+    PageCur: 'home',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //页面跳转时用
   NavChange(e) {
@@ -22,8 +25,53 @@ Page({
   //转发、分享页面的信息
   onShareAppMessage() {
     return {
-      title: '测试用标题',
+      title: '百川卉竹-校园互助平台',
+      imageUrl: '/image/share.jpgs',
       path: '/pages/index/index'
     }
   },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    if(e.detail.userInfo!=null)
+    {
+      app.globalData.userInfo = e.detail.userInfo
+      this.setData({
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true
+      })
+      console.log("用户同意授权！")
+    }
+    else
+    {
+      console.log("用户取消了授权！")
+    }
+  }
 })
